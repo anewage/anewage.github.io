5f7bd5ff0d28dfffb4ffb7a994c30d4f2646753a
Amir Haghighati <haghighati.amir@gmail.com>
2019-01-24 20:13:54 -0500

    update nuxt.config.js and static files

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,15 +6,31 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'Mohammad Rajabi Seraji Homepage',
+    title: 'Amir Haghighati Maleki\'s homepage',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Mohammad Rajabi Seraji Homepage' }
+      { hid: 'description', name: 'description', content: 'Amir Haghighati Maleki\'s personal website.' },
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'msapplication-TileImage', content: '/ms-icon-144x144.png' },
+      { name: 'theme-color', content: '#ffffff' }
     ],
     link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
+      { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-icon-192x192.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: '57x57', href: '/apple-icon-57x57.png' },
+      { rel: 'apple-touch-icon', sizes: '60x60', href: '/apple-icon-60x60.png' },
+      { rel: 'apple-touch-icon', sizes: '72x72', href: '/apple-icon-72x72.png' },
+      { rel: 'apple-touch-icon', sizes: '76x76', href: '/apple-icon-76x76.png' },
+      { rel: 'apple-touch-icon', sizes: '114x114', href: '/apple-icon-114x114.png' },
+      { rel: 'apple-touch-icon', sizes: '120x120', href: '/apple-icon-120x120.png' },
+      { rel: 'apple-touch-icon', sizes: '144x144', href: '/apple-icon-144x144.png' },
+      { rel: 'apple-touch-icon', sizes: '152x152', href: '/apple-icon-152x152.png' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-icon-180x180.png' }
     ]
   },
   plugins: ['~/plugins/vuetify.js'],
@@ -24,12 +40,12 @@ module.exports = {
   /*
   ** Customize the progress bar color
   */
-  loading: { color: '#3B8070' },
+  loading: { color: '#00f9ff' },
 
   modules: [
     // Simple usage
     ['@nuxtjs/google-analytics', {
-      id: 'UA-125968169-1'
+      id: 'UA-109836446-1'
     }]
   ],
 
@@ -59,7 +75,7 @@ module.exports = {
       vueLoader.options.transformToRequire = {
         'img': 'src',
         'image': 'xlink:href',
-        'v-img': 'src',
+        'v-img': 'src'
       }
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
